d43bdd7832b3bf0f30cc6a54bd03b820863c2f0d
Louisii <louisimtd@outlook.com>
2024-06-18 12:08:50 -0300

    cadastrar especialidade

--- Client-MedPlanner/src/auth/CadastroEspecialidade.jsx
+++ Client-MedPlanner/src/auth/CadastroEspecialidade.jsx
@@ -1,31 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import Layout from '../components/Layout'
-import Button from '../components/Button'
-import Input from '../components/Input'
-import Label from '../components/Label'
-import Combobox from '../components/Combobox'
+import Layout from '../components/Layout';
+import Button from '../components/Button';
+import Input from '../components/Input';
+import Label from '../components/Label';
 import axiosWithToken from '../lib/RequestInterceptor';
 import { useNavigate, useParams } from 'react-router-dom';
-import InputDisabled from '../components/InputDisabled';
 
 const CadastroEspecialidade = () => {
-    const { especialidadeId } = useParams()
-    const [form, setForm] = useState({});
+    const { especialidadeId } = useParams();
+    const navigate = useNavigate();
+    const [form, setForm] = useState({ nome: '', sigla: '' });
     const [respostaErro, setRespostaErro] = useState([]);
     const [respostaOk, setRespostaOk] = useState(false);
     const [enviar, setEnviar] = useState(false);
-    const navigate = useNavigate();
     const [especialidade, setEspecialidade] = useState(null);
-
 
     const opcoesUF = ['Selecione', 'RS', 'SC', 'PR', 'SP'];
     const opcoesFuncao = ['Selecione', 'Administrador(a)', 'Recepcionista', 'Médico(a)'];
     const opcoesEspecialidade = ['Selecione', 'Cardiologista', 'Geral'];
     const opcoesSituacao = ['Selecione', 'A', 'I', 'E'];
 
+    useEffect(() => {
+        if (especialidadeId) {
+            getEspecialidade(especialidadeId);
+        }
+    }, [especialidadeId]);
 
-    const getEspecialidade = (especialidadeId) => {
-        axiosWithToken.get(`http://localhost:8080/especialidade/buscar?id=${especialidadeId}`)
+    const getEspecialidade = (id) => {
+        axiosWithToken.get(`http://localhost:8080/especialidade/buscar?id=${id}`)
             .then((response) => {
                 if (response.status === 200) {
                     setEspecialidade(response.data[0]);
@@ -36,139 +38,76 @@ const CadastroEspecialidade = () => {
             .catch((error) => {
                 console.error('Erro ao obter especialidade:', error.message);
             });
-    }
+    };
 
     const salvarEspecialidade = () => {
-        axiosWithToken.post(`http://localhost:8080/especialidade/salvar`, form)
+        axiosWithToken.post('http://localhost:8080/especialidade/salvar', form)
             .then((response) => {
-                if (response.status == 200) {
+                if (response.status === 200) {
                     setRespostaOk(true);
-                    navigate("/listagem-especialidade");
+                    navigate('/listagem-especialidade');
                 }
             })
             .catch((error) => {
-                setRespostaErro(error.response.data['errors']);
-                console.log(respostaErro);
-
-            })
-
-    }
+                setRespostaErro(error.response.data.errors);
+                console.error('Erro ao salvar especialidade:', error.message);
+            });
+    };
 
     const handleForm = (name, value) => {
         setForm({ ...form, [name]: value });
     };
 
     const handleSubmit = () => {
-        if (especialidadeId != null) {
-            setForm({
-                ...{ idEspecialidade: especialidadeId },
-                ...form,
-            });
+        if (especialidadeId) {
+            setForm({ ...form, idEspecialidade: especialidadeId });
         }
-
-        console.log('form');
-        console.log(JSON.stringify(form));
-        salvarEspecialidade()
+        salvarEspecialidade();
         setEnviar(true);
+    };
 
-    }
-
-    useEffect(() => {
-        if (Object.keys(form).length > 0) {
-            salvarEspecialidade();
-        }
-        if (especialidadeId != null) {
-            getEspecialidade(especialidadeId);
-        }
-    }, [enviar, especialidadeId]);
-
-
-
-    return (<Layout>
-        <div className='p-4'>
-            <form>
-
-                <h2 className='p-4'>{especialidadeId != null ? "Edição de Especialidade" : "Cadastro de Especialidade"}</h2>
-
-                <div className='p-4 grid grid-cols-2 gap-8'>
-
-                    <div className='m-4'>
-                        <Label text="Nome Completo" />
-                        <Input type='text' placeholder='' value={especialidade != null && form.nome == null ? especialidade.nome : form.nome} onChange={(e) => handleForm('nome', e.target.value)} />
+    return (
+        <Layout>
+            <div className="p-4 my-auto">
+                <form>
+                    <h2 className="p-4">{especialidadeId ? 'Edição de Especialidade' : 'Cadastro de Especialidade'}</h2>
+                    <div className="p-4 grid grid-cols-2 gap-8">
+                        <div className="m-4">
+                            <Label text="Nome" />
+                            <Input
+                                type="text"
+                                placeholder=""
+                                value={form.nome || (especialidade ? especialidade.nome : '')}
+                                onChange={(e) => handleForm('nome', e.target.value)}
+                            />
+                        </div>
+                        <div className="m-4">
+                            <Label text="Sigla" />
+                            <Input
+                                type="text"
+                                placeholder=""
+                                value={form.sigla || (especialidade ? especialidade.sigla : '')}
+                                onChange={(e) => handleForm('sigla', e.target.value)}
+                            />
+                        </div>
                     </div>
-                    <div className='m-4'>
-                        <Label text="Email" />
-                        <Input type='text' placeholder='' value={especialidade != null && form.username == null ? especialidade.username : form.username} onChange={(e) => handleForm('username', e.target.value)} />
+                    {(!respostaOk && respostaErro.length > 0) && (
+                        <div className="bg-red-300 text-white rounded-md px-4 py-2 mx-8 my-2">
+                            {respostaErro.map((e, index) => (
+                                <p key={index}>{e}</p>
+                            ))}
+                        </div>
+                    )}
+                    <div className="flex gap-4 p-8 items-center justify-end">
+                        {especialidadeId && (
+                            <Button onClick={() => navigate(`/especialidade/${especialidadeId}`)} text="Voltar" />
+                        )}
+                        <Button onClick={handleSubmit} text={especialidadeId ? 'Atualizar' : 'Cadastrar'} />
                     </div>
+                </form>
+            </div>
+        </Layout>
+    );
+};
 
-
-                </div>
-
-                <div className='p-4 grid grid-cols-3 gap-8'>
-                    <div className='m-4'>
-                        <Label text="CPF" />
-                        <Input type='text' placeholder='' value={especialidade != null && form.cpf == null ? especialidade.cpf : form.cpf} onChange={(e) => handleForm('cpf', e.target.value)} />
-                    </div>
-                    <div className='m-4'>
-                        <Label text="Função" />
-                        <Combobox opcoes={opcoesFuncao} opcoesDisplay={opcoesFuncao} value={especialidade != null ? opcoesFuncao[1] : null} onChange={(e) => handleForm('funcao', e.target.value)} />
-                    </div>
-                    <div className='m-4'>
-                        <Label text="Situação" />
-                        {form.idEspecialidade != null ?
-                            <InputDisabled type='text' placeholder='' value={opcoesSituacao[2]} onChange={(e) => handleForm('situacao', e.target.value)} />
-                            :
-                            <Combobox opcoes={opcoesSituacao} opcoesDisplay={opcoesSituacao} value={especialidade != null ? opcoesSituacao[1] : null} onChange={(e) => handleForm('situacao', e.target.value)} />
-                        }
-
-                    </div>
-                </div>
-
-                <div className='p-4 grid grid-cols-3 gap-8'>
-                    <div className='m-4'>
-                        <Label text="Especialidade" />
-                        {form.funcao != opcoesFuncao[3] ?
-                            <InputDisabled type='text' placeholder='' onChange={(e) => handleForm('crm', e.target.value)} />
-                            :
-                            <Combobox opcoes={opcoesEspecialidade} opcoesDisplay={opcoesEspecialidade} onChange={(e) => handleForm('especialidade', e.target.value)} />
-                        }
-                    </div>
-                    <div className='m-4'>
-                        <Label text="CMR" />
-                        {form.funcao != opcoesFuncao[3] ?
-                            <InputDisabled type='text' placeholder='' onChange={(e) => handleForm('crm', e.target.value)} />
-                            :
-                            <Input type='text' placeholder='' onChange={(e) => handleForm('crm', e.target.value)} />
-                        }
-
-
-                    </div>
-                    <div className='m-4'>
-                        <Label text="UF" />
-                        {form.funcao != opcoesFuncao[3] ?
-                            <InputDisabled type='text' placeholder='' onChange={(e) => handleForm('crm', e.target.value)} />
-                            :
-                            <Combobox opcoes={opcoesUF} opcoesDisplay={opcoesUF} onChange={(e) => handleForm('uf_crm', e.target.value)} />
-                        }
-                    </div>
-
-                </div>
-
-
-                {!respostaOk && respostaErro == undefined || respostaErro.length > 0 ?
-                    <div className='bg-red-300 text-white rounded-md px-4 py-2 mx-8 my-2'>
-                        {respostaErro.map((e) => <p>{e}</p>)}
-                    </div>
-                    : null}
-
-                <div className='flex gap-4 p-8 items-center justify-end'>
-                    {especialidadeId != null ? <Button onClick={() => navigate(`/especialidade/${especialidadeId}`)} text="Voltar" /> : null}
-                    <Button onClick={() => handleSubmit()} text={especialidadeId != null ? "Atualizar" : "Cadastrar"} />
-                </div>
-            </form>
-        </div>
-    </Layout>
-    )
-}
-
-export default CadastroEspecialidade
+export default CadastroEspecialidade;
