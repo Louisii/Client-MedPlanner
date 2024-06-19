f871eeaa881dd9bc16060d2c52618bb8484addf1
ahnaoRaquel <niehues.raquel@gmail.com>
2024-06-19 20:39:30 -0300

    Criação de Cadastro de Ala.

---
+++ Client-MedPlanner/src/auth/CadastroAla.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from 'react';
+import Layout from '../components/Layout';
+import Button from '../components/Button';
+import Input from '../components/Input';
+import Label from '../components/Label';
+import axiosWithToken from '../lib/RequestInterceptor';
+import { useNavigate, useParams } from 'react-router-dom';
+
+const CadastroAla = () => {
+    const { alaId } = useParams();
+    const navigate = useNavigate();
+    const [form, setForm] = useState({ nome: '', sigla: '', andar: '' });
+    const [respostaErro, setRespostaErro] = useState([]);
+    const [respostaOk, setRespostaOk] = useState(false);
+    const [enviar, setEnviar] = useState(false);
+    const [ala, setAla] = useState(null);
+
+
+    useEffect(() => {
+        if (alaId) {
+            getAla(alaId);
+        }
+    }, [alaId]);
+
+    const getAla = (id) => {
+        axiosWithToken.get(`http://localhost:8080/ala/buscar?id=${id}`)
+            .then((response) => {
+                if (response.status === 200) {
+                    setForm(response.data[0]);
+                } else {
+                    console.error(`Falha ao obter Ala: ${response.status}`);
+                }
+            })
+            .catch((error) => {
+                console.error('Erro ao obter Ala:', error.message);
+            });
+    };
+
+    const salvarAla = () => {
+        axiosWithToken.post('http://localhost:8080/ala/salvar', form)
+            .then((response) => {
+                if (response.status === 200) {
+                    setRespostaOk(true);
+                    navigate('/listagem-ala');
+                }
+            })
+            .catch((error) => {
+                setRespostaErro(error.response.data.errors);
+                console.error('Erro ao salvar ala:', error.message);
+            });
+    };
+
+    const handleSubmit = () => {
+        if (alaId) {
+            setForm({ ...form, idAla: alaId });
+        }
+        salvarAla();
+        setEnviar(true);
+    };
+
+    return (
+        <Layout>
+            <div className="p-4 my-auto">
+                <form>
+                    <h2 className="p-4">{alaId ? 'Edição de Ala' : 'Cadastro de Ala'}</h2>
+                    <div className="p-4 grid grid-cols-2 gap-8">
+                        <div className="m-4">
+                            <Label text="Nome" />
+                            <Input
+                                type="text"
+                                placeholder=""
+                                value={form.nome || (ala ? ala.nome : '')}
+                                onChange={(e) => handleForm('nome', e.target.value)}
+                            />
+                        </div>
+                        <div className="m-4">
+                            <Label text="Sigla" />
+                            <Input
+                                type="text"
+                                placeholder=""
+                                value={form.sigla || (ala ? ala.sigla : '')}
+                                onChange={(e) => handleForm('sigla', e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    {(!respostaOk && respostaErro.length > 0) && (
+                        <div className="bg-red-300 text-white rounded-md px-4 py-2 mx-8 my-2">
+                            {respostaErro.map((e, index) => (
+                                <p key={index}>{e}</p>
+                            ))}
+                        </div>
+                    )}
+                    <div className="flex gap-4 p-8 items-center justify-end">
+                        {alaId && (
+                            <Button onClick={() => navigate(`/ala/${alaId}`)} text="Voltar" />
+                        )}
+                        <Button onClick={handleSubmit} text={alaId ? 'Atualizar' : 'Cadastrar'} />
+                    </div>
+                </form>
+            </div>
+        </Layout>
+    );
+};
+
+export default CadastroAla;
