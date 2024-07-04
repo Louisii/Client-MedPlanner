d09450592f1bdf1d6f94d8ace83e13c63ce5e815
ahnaoRaquel <niehues.raquel@gmail.com>
2024-07-03 23:50:13 -0300

    formatacao do cpf e correcao de texto

--- Client-MedPlanner/src/auth/DetalhesUsuario.jsx
+++ Client-MedPlanner/src/auth/DetalhesUsuario.jsx
@@ -1,16 +1,19 @@
-
 import React, { useState, useEffect } from 'react';
 import Layout from '../components/Layout';
-import { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom';
 import axiosWithToken from '../lib/RequestInterceptor';
 import Button from '../components/Button';
 
 const DetalhesUsuario = () => {
-    const { usuarioId } = useParams()
+    const { usuarioId } = useParams();
     const [usuario, setUsuario] = useState(null);
     const navigate = useNavigate();
 
-
+    const maskCpf = (cpf) => {
+        if (!cpf) return '';
+        const cpfNumbers = cpf.replace(/\D/g, '');
+        return cpfNumbers.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+    };
 
     useEffect(() => {
         getUsuario(usuarioId);
@@ -29,36 +32,30 @@ const DetalhesUsuario = () => {
                 console.error('Erro ao obter usuário:', error.message);
                 // Aqui você pode adicionar lógica para exibir uma mensagem de erro para o usuário
             });
-    }
+    };
 
     return (
         <Layout>
             {usuario != null ?
                 <div className='p-4'>
                     <h2 className='p-4 text-xl font-bold'>Detalhes do Usuário</h2>
-
-
                     <div className='m-4 p-4'>
-                        {/* <h2 className='text-md font-bold'>{usuario.nome}</h2> */}
-                        <p>Username: {usuario.nome}</p>
-                        <p>Função: {usuario.cargo == 'RECEPCAO' ? 'Recepção' : usuario.cargo == 'MEDICO' ? 'Médico(a)' : 'Administrador(a)'}</p>
-                        <p>Username: {usuario.username}</p>
-                        <p>CPF: {usuario.cpf}</p>
+                        <p>Nome: {usuario.nome}</p>
+                        <p>Função: {usuario.cargo === 'RECEPCAO' ? 'Recepção' : usuario.cargo === 'MEDICO' ? 'Médico(a)' : 'Administrador(a)'}</p>
+                        <p>Email: {usuario.username}</p>
+                        <p>CPF: {maskCpf(usuario.cpf)}</p>
                         <p>Situação: {usuario.situacao}</p>
                     </div>
-                    <div className='flex items-center text-sm  h-9 gap-4 m-4 p-4'>
+                    <div className='flex items-center text-sm h-9 gap-4 m-4 p-4'>
                         <Button onClick={() => navigate(`/listagem-usuario`)} text="Voltar" />
                         <Button onClick={() => navigate(`/edicao-usuario/${usuarioId}`)} text="Editar" />
                     </div>
-
-
                 </div>
                 :
                 <p></p>
             }
-        </Layout >
-    )
+        </Layout>
+    );
+};
 
-}
-
-export default DetalhesUsuario
+export default DetalhesUsuario;
