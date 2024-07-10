9a1f3342cbba578eba6409a20d529e5669258878
Raquel Niehues <niehues.raquel@gmail.com>
2024-07-10 15:46:26 -0300

    Correção do bug de atualização de páginas.

--- Client-MedPlanner/src/lib/RequestInterceptor.js
+++ Client-MedPlanner/src/lib/RequestInterceptor.js
@@ -2,11 +2,10 @@ import axios from "axios";
 
 // interceptador de requisições. Utilizado para adicionar token para as requisições
 
-const token = sessionStorage.getItem("token");
-
 const axiosWithToken = axios.create();
 axiosWithToken.interceptors.request.use(
     config => {
+        const token = sessionStorage.getItem("token");
         if (token) {
             config.headers.Authorization = "Bearer " + token;
             config.headers['Cache-Control'] = 'no-cache';
@@ -15,18 +14,5 @@ axiosWithToken.interceptors.request.use(
     },
     error => Promise.reject(error)
 );
-
-// axiosWithToken.interceptors.response.use(
-//     response => {
-//         // console.log(response.data);
-//         return response;
-//     },
-//     error => {
-//         if (error.response)
-//             console.log("Erro em RequestInterceptor: " + error);
-//         else
-//             return Promise.reject(error);
-//     }
-// );
 
 export default axiosWithToken;
